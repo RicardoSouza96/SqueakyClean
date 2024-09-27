11f69cef41a293d036ca317593bedc23f6eb5a74
Ricardo Souza <ricksouz96@gmail.com>
2024-09-26 21:12:16 -0300

    Replaces whitespace with underscores

--- SqueakyClean.cs
+++ SqueakyClean.cs
@@ -4,7 +4,13 @@ public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        char[] chars = identifier.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] == ' ')
+                chars[i] = '_';
+        }
+        return new string(chars);
     }
 }
 

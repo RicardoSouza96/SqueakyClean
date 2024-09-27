f498530225e5f3f8e6120d8fa0f131f8fcc5c3c4
Ricardo Souza <ricksouz96@gmail.com>
2024-09-26 21:10:42 -0300

    Add project files.

---
+++ SqueakyClean.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+    }
+}
+
+
